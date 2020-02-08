0eaa40d8e471ceb7d8cafd9018d74a442aaa5686
Mason Eyre <mason.deyre@gmail.com>
2020-02-07 17:10:33 -0700

    Figuring navbars out

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,167 +1,156 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>First Laravel App</title>
+@section('main')
+  <style>
+    html, body {
+      background-color: #fff;
+      color: #FFFFFF;
+      font-family: 'Helveticca', sans-serif;
+      font-weight: 200;
+      height: 100vh;
+      margin: 0;
+    }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    .full-height {
+      height: 100vh;
+    }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-              background-color: #fff;
-              color: #FFFFFF;
-              font-family: 'Helveticca', sans-serif;
-              font-weight: 200;
-              height: 100vh;
-              margin: 0;
-            }
+    .flex-center {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
 
-            .full-height {
-              height: 100vh;
-            }
+    .position-ref {
+      position: relative;
+    }
 
-            .flex-center {
-              align-items: center;
-              display: flex;
-              justify-content: center;
-            }
+    .top-right {
+      position: absolute;
+      right: 20px;
+      top: 20px;
+    }
 
-            .position-ref {
-              position: relative;
-            }
+    .main {
+      text-align: center;
+      background-color: #2A2A2A;
+      padding: 70px;
+      border-radius: 75px;
+      margin: 50px;
+      padding-bottom: 20px;
+    }
 
-            .top-right {
-              position: absolute;
-              right: 20px;
-              top: 20px;
-            }
+    .title {
+      font-size: 70px;
+    }
 
-            .content {
-              text-align: center;
-              background-color: #2A2A2A;
-              padding: 70px;
-              border-radius: 75px;
-              margin: 50px;
-              padding-bottom: 20px;
-            }
+    .links > a {
+      color: #FFFFFF;
+      padding: 0 25px;
+      font-size: 13px;
+      font-weight: 600;
+      letter-spacing: .1rem;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+    .grid > a{
+      color: #FFFFFF;
+      padding: 0 25px;
+      font-size: 13px;
+      font-weight: 500;
+      letter-spacing: .1rem;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+    .grid {
+      display: flex;
+      flex-wrap: wrap;
+      display: grid;
+      grid-template-columns: repeat(2, minmax(20px, auto));
+      grid-auto-rows: minmax(20px, auto);
+      grid-gap: 30px;
+    }
+    .module {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 20px;
+      margin-left: 5px;
+      margin-right: 5px;
+      flex: 1 1 100px;
+    }
 
-            .title {
-              font-size: 70px;
-            }
+    .m-b-md {
+      margin-bottom: 30px;
+    }
+    .background{
+      background-color: #808080;
+      height: calc(100vh);
+      overflow-y:visible;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+    }
 
-            .links > a {
-              color: #FFFFFF;
-              padding: 0 25px;
-              font-size: 13px;
-              font-weight: 600;
-              letter-spacing: .1rem;
-              text-decoration: none;
-              text-transform: uppercase;
-            }
-            .grid > a{
-              color: #FFFFFF;
-              padding: 0 25px;
-              font-size: 13px;
-              font-weight: 500;
-              letter-spacing: .1rem;
-              text-decoration: none;
-              text-transform: uppercase;
-            }
-            .grid {
-              display: flex;
-              flex-wrap: wrap;
-              display: grid;
-              grid-template-columns: repeat(2, minmax(20px, auto));
-              grid-auto-rows: minmax(20px, auto);
-              grid-gap: 30px;
-            }
-            .module {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              height: 20px;
-              margin-left: 5px;
-              margin-right: 5px;
-              flex: 1 1 100px;
-            }
+  </style>
+  <body class='background'>
+    <div class="flex-center position-ref full-height">
+      @if (Route::has('login'))
+        <div class="top-right links">
+          @auth
+            <a href="{{ url('/home') }}">Home</a>
+          @else
+            <a href="{{ route('login') }}">Login</a>
 
-            .m-b-md {
-              margin-bottom: 30px;
-            }
-            .background{
-              background-color: #808080;
-              height: calc(100vh);
-              overflow-y: auto;
-              background-size: cover;
-              background-position: center;
-              background-repeat: repeat;
-              padding-bottom: 120px;
-            }
-
-        </style>
-    </head>
-    <body class='background'>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+            @if (Route::has('register'))
+              <a href="{{ route('register') }}">Register</a>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    My First Laravel App
-                </div>
-
-                <p class='links'>
-                  <a href="http://www.masoneyre.com" target='_blank'>
-                    <u>-My Website-</u>
-                  </a>
-                </p>
-                <br/>
-                <h2>My Projects</h2>
-                <div class="grid">
-                    <a href="https://appestimation.herokuapp.com/" target='_blank' class='module'>
-                      <u>-App Estimator-</u>
-                    </a>
-                    <a href="http://triptrackplan.herokuapp.com/" target='_blank' class='module'>
-                      <u>-Trip Tracker-</u>
-                    </a>
-                    <a href="http://www.ceojanitorial.com/" target='_blank' class='module'>
-                      <u>-CEO Janitorial-</u>
-                    </a>
-                    <a href="https://repl.it/@MasonMan/Ruby-Store" target='_blank' class='module'>
-                      <u>-Ruby Store-</u>
-                    </a>
-                </div>
-                <br/>
-                <p class='links'>
-                  You can see more on my GitHub!
-                  <br/>
-                  <a href="https://github.com/DudeMason" target='_blank'>
-                    <u>-GitHub-</u>
-                  </a>
-                </p>
-            </div>
-            <p style="opacity:.6; position: absolute; margin-left: auto; margin-right: auto; left: 0; right: 0; bottom: 0%; margin-bottom: -60px;text-align: center;">
-              Created by Mason Eyre
-              <br/>
-              <a href="http://www.masoneyre.com" target='_blank'>
-                www.masoneyre.com
-              </a>
-            </p>
+          @endauth
         </div>
-    </body>
-</html>
+      @endif
+
+      <div class="main">
+        <div class="title m-b-md">
+          My First Laravel App
+        </div>
+
+          <p class='links'>
+            <a href="http://www.masoneyre.com" target='_blank'>
+              <u>-My Website-</u>
+            </a>
+          </p>
+          <br/>
+          <h2>My Projects</h2>
+          <div class="grid">
+            <a href="https://appestimation.herokuapp.com/" target='_blank' class='module'>
+              <u>-App Estimator-</u>
+            </a>
+            <a href="http://triptrackplan.herokuapp.com/" target='_blank' class='module'>
+              <u>-Trip Tracker-</u>
+            </a>
+            <a href="http://www.ceojanitorial.com/" target='_blank' class='module'>
+              <u>-CEO Janitorial-</u>
+            </a>
+            <a href="https://repl.it/@MasonMan/Ruby-Store" target='_blank' class='module'>
+              <u>-Ruby Store-</u>
+            </a>
+          </div>
+          <br/>
+          <p class='links'>
+            You can see more on my GitHub!
+            <br/>
+            <a href="https://github.com/DudeMason" target='_blank'>
+              <u>-GitHub-</u>
+            </a>
+          </p>
+      </div>
+      <p style="opacity:.6; position: absolute; margin-left: auto; margin-right: auto; left: 0; right: 0; bottom: 0%; margin-bottom: -60px;text-align: center;">
+        Created by Mason Eyre
+        <br/>
+        <a href="http://www.masoneyre.com" target='_blank'>
+          www.masoneyre.com
+        </a>
+      </p>
+    </div>
+  </body>
+@endsection
