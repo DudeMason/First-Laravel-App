e79e3040c231d50fd8811692fd3bd1ba6871ed8a
Mason Eyre <mason.deyre@gmail.com>
2020-02-07 22:02:06 -0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,6 +7,12 @@
       <div class='title m-b-md'>
         About Page
       </div>
+      <p>
+        I'm really diggin' this Laravel stuff. I think I might stick with it!!
+        <br/>
+        <br/>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      </p>
     </div>
   </div>
 </body>
